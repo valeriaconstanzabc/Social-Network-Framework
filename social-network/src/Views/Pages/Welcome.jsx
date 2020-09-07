831d97471478b094b777198cbbc5439d10451cc6
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-09-07 20:09:42 -0300

    elimino contenedor sobrante vista welcome

--- social-network/src/Views/Pages/Welcome.jsx
+++ social-network/src/Views/Pages/Welcome.jsx
@@ -3,18 +3,16 @@ import '../Styles/Welcome.css'
 
 const Welcome = () => {
     return (
-        <div className="containerBackground1">
-            <div className="welcomeLofche">
-                <div className="divLogoWelcome">
-                    <img src="https://raw.githubusercontent.com/valeriaconstanzabc/SCL013-social-network/master/src/imagenes/lofcheColgante.png" alt="Logo bienvenida" className="logoWelcome"/>
-                </div>
-                <div className="containerWelcome">
-                    <p>Lofche significa "Comunidad" en Mapudungun. Y eso es 
-                    lo que somos! una comunidad para averiguar todas aquellas 
-                    cosas que no sabemos, aquellas cosas que buscamos y no 
-                    encontramos. Ayuda y conexión constante con tu ciudad y las 
-                    almas que viven en ella!</p>
-                </div>
+        <div className="welcomeLofche">
+            <div className="divLogoWelcome">
+                <img src="https://raw.githubusercontent.com/valeriaconstanzabc/SCL013-social-network/master/src/imagenes/lofcheColgante.png" alt="Logo bienvenida" className="logoWelcome"/>
+            </div>
+            <div className="containerWelcome">
+                <p>Lofche significa "Comunidad" en Mapudungun. Y eso es 
+                lo que somos! una comunidad para averiguar todas aquellas 
+                cosas que no sabemos, aquellas cosas que buscamos y no 
+                encontramos. Ayuda y conexión constante con tu ciudad y las 
+                almas que viven en ella!</p>
             </div>
         </div>
     )
