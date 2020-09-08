e1be3dd8d828127e888260d1364a3e76c77d9278
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-09-08 20:50:42 -0300

    nuevas variables sass + implementación

--- social-network/src/Views/Components/HeaderWelcome.jsx
+++ social-network/src/Views/Components/HeaderWelcome.jsx
@@ -4,16 +4,18 @@ import { Link } from "react-router-dom";
 
 const HeaderWelcome = () => {
     return (
-        <div className="header">
-            <div className="headerLogo">
-                <Link to="/">
-                    <img src="https://raw.githubusercontent.com/valeriaconstanzabc/SCL013-social-network/master/src/imagenes/Logo.png" alt="logo-lofche" className="logoHeader"/>
-                </Link>
-            </div>
-            <div className="divLogInButton">
-                <Link to="/iniciarSesion">
-                    <button className="logInButton">Iniciar Sesión</button>
-                </Link>
+        <div className="containerHeader">
+            <div className="header">
+                <div className="headerLogo">
+                    <Link to="/">
+                        <img src="https://raw.githubusercontent.com/valeriaconstanzabc/SCL013-social-network/master/src/imagenes/Logo.png" alt="logo-lofche" className="logoHeader"/>
+                    </Link>
+                </div>
+                <div className="divLogInButton">
+                    <Link to="/iniciarSesion">
+                        <button className="logInButton">Iniciar Sesión</button>
+                    </Link>
+                </div>
             </div>
         </div>
     )
