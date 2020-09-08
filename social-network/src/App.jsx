7b5f56c4fb7f5394cee028249322da1623ee085a
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-09-08 15:47:16 -0300

    agrego component header

--- social-network/src/App.jsx
+++ social-network/src/App.jsx
@@ -5,14 +5,18 @@ import {
   Route
 } from "react-router-dom";
 import Welcome from './Views/Pages/Welcome';
+import HeaderWelcome from './Views/Components/HeaderWelcome';
 
 function App() {
   return (
       <Router>
         <Switch>
+          
           <Route path="/" exact>
+            <HeaderWelcome />
             <Welcome />
           </Route>
+
         </Switch>
       </Router>
   );
---
+++ social-network/src/Views/Components/HeaderWelcome.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../Styles/HeaderWelcome.css'
+import { Link } from "react-router-dom";
+
+const HeaderWelcome = () => {
+    return (
+        <div className="header">
+            <div className="headerLogo">
+                <Link to="/">
+                    <img src="https://raw.githubusercontent.com/valeriaconstanzabc/SCL013-social-network/master/src/imagenes/Logo.png" alt="logo-lofche" className="logoHeader"/>
+                </Link>
+            </div>
+            <div className="divLogInButton">
+                <Link to="/iniciarSesion">
+                    <button className="logInButton">Iniciar Sesión</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderWelcome
