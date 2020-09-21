21d5d05503c816c241126ee0066819897d55871e
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-09-21 15:02:16 -0300

    cambio de fondos

--- social-network/src/Views/Pages/Welcome.jsx
+++ social-network/src/Views/Pages/Welcome.jsx
@@ -11,16 +11,18 @@ const Welcome = (props) => {
     }
 
     return (
-        <div className="welcomeLofche">
-            <div className="divLogoWelcome">
-                <img src="https://raw.githubusercontent.com/valeriaconstanzabc/SCL013-social-network/master/src/imagenes/lofcheColgante.png" alt="Logo bienvenida" className="logoWelcome"/>
-            </div>
-            <div className="containerWelcome">
-                <p>Lofche significa "Comunidad" en Mapudungun. Y eso es 
-                lo que somos! una comunidad para averiguar todas aquellas 
-                cosas que no sabemos, aquellas cosas que buscamos y no 
-                encontramos. Ayuda y conexión constante con tu ciudad y las 
-                almas que viven en ella!</p>
+        <div className="containerBackground">
+            <div className="welcomeLofche">
+                <div className="divLogoWelcome">
+                    <img src="https://raw.githubusercontent.com/valeriaconstanzabc/SCL013-social-network/master/src/imagenes/lofcheColgante.png" alt="Logo bienvenida" className="logoWelcome"/>
+                </div>
+                <div className="containerWelcome">
+                    <p>Lofche significa "Comunidad" en Mapudungun. Y eso es 
+                    lo que somos! una comunidad para averiguar todas aquellas 
+                    cosas que no sabemos, aquellas cosas que buscamos y no 
+                    encontramos. Ayuda y conexión constante con tu ciudad y las 
+                    almas que viven en ella!</p>
+                </div>
             </div>
         </div>
     )
